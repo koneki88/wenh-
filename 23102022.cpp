ed4f0b129c749965afaf1b295935ac9d633ceb68
koneki88 <114171959+koneki88@users.noreply.github.com>
2022-10-23 19:05:11 +0300

    Add files via upload

---
+++ 23102022.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+int main () {
+
+	/*Series1◦  .Даны десять вещественных чисел.Найти их сумму.
+    float  rez = 0, r;
+    int i;
+    for (i = 1; i <= 10; ++i) {
+        cout << "i" << i << "-";
+        cin >> r;
+        rez += r;
+    }
+    cout << rez;*/
+
+    /*2
+
+    float  rez = 1, r;
+    int i;
+    for (i = 1; i <= 10; ++i) {
+        cout << "i" << i << "-";
+        cin >> r;
+        rez *= r;
+    }
+    cout << rez;*/
+
+
+}
+
+
+
